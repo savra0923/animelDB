43f79585c1cfac6f5ac8b49ba8f485665fdb3db0
savra0923 <savra0923@gmail.com>
2020-08-27 13:30:56 +0300

    partitioned the json based on 'frame_index' and placed it into a dict. cleaned up the pandas dataframe (it now uses multi-indexing), and created a new column that houses the information we need from the .csv in the way we need it.

--- animel_main.py
+++ animel_main.py
@@ -2,7 +2,7 @@ import json
 import pandas as pd
 
 def get_info_form_json(json_line):
-    print(json_line)
+    #print(json_line)
 
     new_json= {}
     new_json['frame_index']= json_line['frame_index']
@@ -21,29 +21,69 @@ def get_info_form_json(json_line):
         'rate': json_line['dogs'][0]['rate']
 
     })
-    print(new_json)
+    #print(new_json)
     #print(aeden_json['frame_annotations'])
-    print("hi!!!")
+    #print("hi!!!")
     r= json.dumps(new_json)
     loader_r= json.loads(r)
     return loader_r
 
+def json_to_dict(aeden_json):
+    jdict = {}
+
+    for index in aeden_json['frame_annotations']:
+        id= aeden_json['frame_annotations'][index]['frame_index'];
+        jdict.update({id: aeden_json['frame_annotations'][index]})
+    return jdict
+
+def get_keypoints_json(row):
+    jkp= {
+        "head": {
+            "x1": row[0],
+            "y1": row[1],
+            "rate": row[2]
+        },
+        "nose": {
+            "x1": row[3],
+            "y1": row[4],
+            "rate": row[5]
+        },
+        "coccyx": {
+            "x1": row[6],
+            "y1": row[7],
+            "rate": row[8]
+        },
+        "Tail": {
+            "x1": row[9],
+            "y1": row[10],
+            "rate": row[11]
+        },
+    }
+    return jkp;
 
 if __name__ == '__main__':
     aeden_pd=pd.read_csv("Aeden_session_1_trial_1.csv")
-    aeden_pd.columns=aeden_pd.iloc[0]
-    aeden_pd.drop(aeden_pd.index[0])
-    #aeden_json=pd.read_json('Aeden_session_1_trial_1.json')
+
+    index= aeden_pd.head(1).combine_first(aeden_pd.head(2)).T
+    #print(index)
+    aeden_pd.columns= index
+    #aeden_pd.rename_axis('data').reset_index()
+
+    #aeden_pd.columns=(aeden_pd.iloc[0], aeden_pd.iloc[1])
+    aeden_pd= aeden_pd.iloc[2:]
+    aeden_pd['bodyparts', 'coords'] = aeden_pd['bodyparts', 'coords'].astype(int)
+    aeden_pd= aeden_pd.set_index(aeden_pd['bodyparts', 'coords'])
+    aeden_pd = aeden_pd.drop(aeden_pd.columns[0], axis=1)
+    #aeden_pd= aeden_pd.drop(('bodyparts', 'coords'), axis = 1, inplace = True)
 
     with open('Aeden_session_1_trial_1.json') as jf:
         aeden_json= json.load(jf)
 
-    #print(aeden_pd)
-    #print(aeden_json)
-
-    #print(aeden_pd.loc[1])
-    #print(aeden_pd.loc[2])
-    #print(aeden_json)
+    print(aeden_pd)
+    #print(aeden_pd['bodyparts', 'coords'])
+    print(aeden_pd.loc[0])
+    json_dict= json_to_dict(aeden_json)
+    print(json_dict)
 
     new_json= {}
     new_json['existed_task']=[]
@@ -53,10 +93,16 @@ if __name__ == '__main__':
     new_json['height'] = 720
     new_json['frame_annotations']= {}
 
-    for index in aeden_json['frame_annotations']:
-        new_json['frame_annotations'].update({
-            index: get_info_form_json(aeden_json['frame_annotations'][index])
-        })
+    aeden_pd['keypoints']= aeden_pd.apply(get_keypoints_json, axis=1)
+    aeden_pd.to_csv('trying.csv')
+
+
+    #for index in json_dict:
+        #print(json_dict[index])
+
+        #new_json['frame_annotations'].update({
+        #    index: get_info_form_json(aeden_json['frame_annotations'][index])
+        #})
 
         #new_json['frame_annotations'].append({
         #    index: get_info_form_json(aeden_json['frame_annotations'][index])
